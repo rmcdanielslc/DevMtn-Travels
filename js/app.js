db99bbc9c423bdfe29b8e347b26b81c482ff433c
rmcdanielslc <rmcdnaielslc@gmail.com>
2017-02-22 10:21:02 -0700

    booked ctrl

--- js/app.js
+++ js/app.js
@@ -4,6 +4,28 @@ angular.module('devmtnTravel', ['ui.router'])
             .state('home',{
                 url:'/',
                 templateUrl: "../views/about.html"
+            })
+            state('packages',{
+                url:'/packages',
+                templateUrl: "../views/packages.html"
+            })
+            state('locations',{
+                url:'/locations',
+                templateUrl: "../views/locations.html"
+            })
+            state('booked',{
+                url:'/booked/:id',
+                templateUrl: "../views/booked.html"
+            })
+            state('adventurers',{
+                url:'/adventurers',
+                parent: 'home',
+                templateUrl: "../views/about-adventurers.html"
+            })
+            state('contact',{
+                url:'/contact',
+                parent: 'home',
+                templateUrl: "../views/contact.html"
             });
 
         $urlRouterProvider
